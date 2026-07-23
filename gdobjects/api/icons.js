fe43e9757214f62b2f1bc2bf5fba60f6374ba949
Triforce <dawes.tyler.gaming@gmail.com>
2026-07-23 17:17:58 -0400

    Create icons.js

---
+++ gdobjects/api/icons.js
@@ -0,0 +1,64 @@
+{
+  "iconCounts": {
+    "tab1": 870,
+    "tab2": 55,
+    "tab3": 144,
+    "tab4": 45,
+    "tab5": 193,
+    "tab6": 50,
+    "tab7": 152,
+    "tab8": 1590,
+    "tab9": 150,
+    "tab10": 212,
+    "tab11": 241,
+    "tab12": 98,
+    "tab13": 126
+  },
+  "forms": {
+    "tab1": {
+      "form": "tab1",
+      "name": "Tab1",
+      "index": 21
+    },
+    "tab2": {
+      "form": "tab2",
+      "name": "Tab2",
+      "index": 22
+    },
+    "tab3": {
+      "form": "tab3",
+      "name": "Tab3",
+      "index": 23
+    },
+    "tab4": {
+      "form": "tab4",
+      "name": "Tab4",
+      "index": 24
+    },
+    "tab5": {
+      "form": "tab5",
+      "name": "Tab5",
+      "index": 25
+    },
+    "tab6": {
+      "form": "tab6",
+      "name": "Tab6",
+      "index": 26
+    },
+    "tab7": {
+      "form": "tab7",
+      "name": "Tab7",
+      "index": 43
+    },
+    "tab8": {
+      "form": "tab8",
+      "name": "Tab8",
+      "index": 53
+    },
+    "tab9": {
+      "form": "tab9",
+      "name": "Tab9",
+      "index": 54
+    }
+  }
+}
