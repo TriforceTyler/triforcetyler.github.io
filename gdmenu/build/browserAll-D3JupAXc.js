7a39a9200ca8ea507f1d7e19da068a25eb7c7688
Triforce <dawes.tyler.gaming@gmail.com>
2026-08-23 16:50:25 -0400

    Create browserAll-D3JupAXc.js

---
+++ gdmenu/build/browserAll-D3JupAXc.js
@@ -0,0 +1,15 @@
+import { dt as extensions } from "./Geometry-CxC-ucSI.js";
+import { l as Container } from "./Filter-DXDffhRK.js";
+import { a as EventSystem, c as AccessibilitySystem, i as FederatedContainer, o as DOMPipe, s as accessibilityTarget } from "./index-DXT6PGz_.js";
+import "./init-RegnnAR_.js";
+//#region node_modules/pixi.js/lib/accessibility/init.mjs
+extensions.add(AccessibilitySystem);
+extensions.mixin(Container, accessibilityTarget);
+//#endregion
+//#region node_modules/pixi.js/lib/dom/init.mjs
+extensions.add(DOMPipe);
+//#endregion
+//#region node_modules/pixi.js/lib/events/init.mjs
+extensions.add(EventSystem);
+extensions.mixin(Container, FederatedContainer);
+//#endregion
