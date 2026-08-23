3897423b7bd5af63ad90c96d495a0825e042f4dc
Triforce <dawes.tyler.gaming@gmail.com>
2026-08-23 16:48:34 -0400

    Create init-RegnnAR_.js

---
+++ gdmenu/build/init-RegnnAR_.js
@@ -0,0 +1,6 @@
+import { dt as extensions } from "./Geometry-CxC-ucSI.js";
+import { n as FilterPipe, r as CanvasFilterSystem, t as FilterSystem } from "./index-DXT6PGz_.js";
+//#region node_modules/pixi.js/lib/filters/init.mjs
+extensions.add(FilterSystem, CanvasFilterSystem);
+extensions.add(FilterPipe);
+//#endregion
